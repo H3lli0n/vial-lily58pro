329a84c51c5a958f7c33a6412e279e56cea0affa
H3lli0n <thulium.hqfr@gmail.com>
2023-12-15 23:07:21 +0100

    Fix animation flickering and add Luna pet as default animation

--- lily58/rev1/keymaps/bongovial/config.h
+++ lily58/rev1/keymaps/bongovial/config.h
@@ -37,12 +37,32 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_LEFT
 // #define MASTER_RIGHT
 #define EE_HANDS 
-#define SPLIT_USB_DETECT
+#define SPLIT_USB_TIMEOUT 2000
+#define USE_SERIAL_PD2
 
 #ifdef OLED_ENABLE
 #    define SPLIT_OLED_ENABLE
+#    define OLED_TIMEOUT 300000
+#    define OLED_UPDATE_INTERVAL 10
 #endif
 
 #ifdef WPM_ENABLE
 #    define SPLIT_WPM_ENABLE
 #endif
+
+#define ANIM_FRAME_DURATION 200
+#define LUNA_PET
+
+/* Luna Pet by helltm */
+#ifdef LUNA_PET
+#    define ANIM_SIZE 96
+#    define MIN_WALK_SPEED      10
+#    define MIN_RUN_SPEED       40
+#else
+#    define ANIM_SIZE 512
+#    define IDLE_FRAMES 5
+#    define IDLE_SPEED 30
+#    define TAP_FRAMES 2
+#    define TAP_SPEED 40
+#endif
+
